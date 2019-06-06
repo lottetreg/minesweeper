9e73b4d2af47294e1693382e8933b19064ff2f16
Charlotte Tregelles <charlotte@8thlight.com>
2019-06-06 10:40:16 -0400

    refactor board to use nested list and atom in empty tile

--- lib/board_presenter.ex
+++ lib/board_presenter.ex
@@ -1,18 +1,18 @@
 defmodule BoardPresenter do
   def present(board) do
     rows_with_numbers =
-      Enum.map(board, fn {row_number, row} ->
+      Enum.map(Enum.with_index(board), fn {row, row_number} ->
         Enum.reduce(row, "#{row_number} |", &present_tile/2) <> "\n"
       end)
 
     ["   A B C D E F G H I J\n", rows_with_numbers]
   end
 
-  defp present_tile({_, %{selected: true}}, acc) do
+  defp present_tile(:selected, acc) do
     acc <> "X|"
   end
 
-  defp present_tile({_, %{selected: false}}, acc) do
+  defp present_tile(:empty, acc) do
     acc <> " |"
   end
 end
--- lib/game.ex
+++ lib/game.ex
@@ -1,17 +1,33 @@
 defmodule Game do
-  import GameHelpers
-
   def play(%GameState{status: :in_progress} = game_state, game_rules) do
     print_board(game_state.config.writer, game_state.board)
 
     move = get_move(game_state.config.reader)
-    board = select_board_tile(game_state.board, move)
-    game_state = GameState.set_board(game_state, board)
+    board = Board.select_tile(game_state.board, move)
 
-    play(update_status(game_state, game_rules), game_rules)
+    game_state
+    |> GameState.set_board(board)
+    |> update_game_state_status(game_rules)
+    |> play(game_rules)
   end
 
   def play(%GameState{status: :over} = game_state, _) do
     print_board(game_state.config.writer, game_state.board)
+  end
+
+  defp update_game_state_status(game_state, game_rules) do
+    if game_rules.over? do
+      GameState.set_status(game_state, :over)
+    else
+      game_state
+    end
+  end
+
+  defp print_board(writer, board) do
+    writer.write(BoardPresenter.present(board))
+  end
+
+  defp get_move(reader) do
+    Move.translate(reader.read)
   end
 end
